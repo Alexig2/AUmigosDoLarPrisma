2440e9d7e6721d46b9be7775b3541341f840629d
Lucas Alves dos Santos <lucas_ads@hotmail.com.br>
2024-10-12 11:18:05 -0400

    Sistema de autenticação básico

--- index.js
+++ index.js
@@ -1,15 +1,54 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-async function main(){
-    const newAdmin = await prisma.admin.create({
-        data: {
-          email: 'alex@example.com',
-         senha: '123',
-        }
-      });
-    
-      console.log('Novo usuário:', newAdmin);
-}
+const express = require("express");
+const exphbs = require("express-handlebars");
 
-main()
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
+
+const authRoutes = require("./routes/authRoutes");
+const AuthController = require("./controllers/AuthController");
+
+const app = express();
+
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Definição e configuração da sessão
+
+app.use(
+  session({
+    name: "session",
+    secret: process.env.SECRET_SESSION,
+    resave: false,
+    saveUninitialized: false,
+    store: new FileStore({
+      logFn: function () {},
+      path: require("path").join(__dirname, "sessions"),
+    }),
+    cookie: {
+      secure: false,
+      maxAge: 3600000,
+      httpOnly: true,
+    },
+  })
+);
+
+// O "AuthController.makeAuthMiddleware" faz com que a página home só possa ser acessada caso o usuário esteja logado.
+app.get("/", AuthController.makeAuthMiddleware, async (req, res) => {
+  const usuario = await prisma.usuario.findUnique({
+    where: {
+      id: req.session.userId,
+    },
+  });
+
+  res.render("home", { usuario });
+});
+
+app.use("/auth/", authRoutes);
+
+app.listen(8000);
